205306c053a8d7e8970d33a3fe2e6351ec4001ea
Marcelo Giorgi <marklazz.uy@gmail.com>
2010-08-21 19:35:07 -0300

    More test for FormOptionsHelper

--- test/form_options_helper_test.rb
+++ test/form_options_helper_test.rb
@@ -15,11 +15,25 @@ class FormOptionsHelperTest < ActionView::TestCase
     assert grouped_options_for_select([["Hats", ["Baseball Cap","Cowboy Hat"]]]).html_safe?
   end
 
+  def test_grouped_options_for_select_with_script_tags_still_returns_html_safe_string
+    assert grouped_options_for_select([["<script>some</script>", ["<script>evil<script>","<script>code!</script>"]]]).html_safe?
+  end
+
+  def test_grouped_options_for_select_prompt_is_escaped
+    grouped_options_result = grouped_options_for_select(grouped_options_sample_data, 'Europe', 'Some unescaped <script>text.</script>')
+    assert !grouped_options_result.match(/<script>/)
+  end
+
   private
 
     def dummy_continents
       [ Continent.new("<Africa>", [Country.new("<sa>", "<South Africa>"), Country.new("so", "Somalia")] ),
        Continent.new("Europe", [Country.new("dk", "Denmark"), Country.new("ie", "Ireland")] ) ]
+    end
+
+    def grouped_options_sample_data
+      [ ['North America', [['United States','US'],'Canada']],
+        ['Europe', ['Denmark','Germany','France']]]
     end
 
 end
